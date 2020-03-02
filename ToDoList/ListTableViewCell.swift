fe3fad1103a0c4d6b8927f73ef4af1932eddb715
Christopher Greene <greenecr@bc.edu>
2020-03-02 18:52:57 -0500

    5.11 completed

---
+++ ToDoList/ListTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ListTableViewCell.swift
+//  ToDoList
+//
+//  Created by Christopher Greene on 3/2/20.
+//  Copyright © 2020 Christopher Greene. All rights reserved.
+//
+
+import UIKit
+
+protocol ListTableViewCellDelegate: class {
+    func checkBoxToggle(sender: ListTableViewCell)
+}
+
+class ListTableViewCell: UITableViewCell {
+    
+    weak var delegate: ListTableViewCellDelegate?
+    
+    @IBOutlet weak var checkBoxButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    @IBAction func checkToggled(_ sender: UIButton) {
+        delegate?.checkBoxToggle(sender: self)
+    }
+    
+}
